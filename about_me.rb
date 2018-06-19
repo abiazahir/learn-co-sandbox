c155fc3471e08bf8476f6748625fceb038b9bfcc
abiazahir <abiazahir@hotmail.com>
2018-06-19 19:20:12 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,9 @@
+def about_me
+  puts "My name is Abia"
+  puts "I grew up in Alexandria"
+  puts "I'm 15 years old"
+  puts "My favorite food is pizza"
+  puts "I was lightning before the thunder"
+end
+
+about_me
---
+++ conditionals.rb
@@ -0,0 +1,12 @@
+time=12.05
+
+
+if time >= 7 && time<= 11
+  puts "Good morning!"
+  elsif time >11 && time<= 17
+  puts "Good afternoon!"
+  elsif time >17 && time<=22
+  puts "Good evening!"
+else
+  puts "Good night!"
+end
