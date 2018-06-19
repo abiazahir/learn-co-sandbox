d23b8d8e048feaf07e6f89e3ac365473260dc505
abiazahir <abiazahir@hotmail.com>
2018-06-19 19:37:47 +0000

    automatically backed up by learn

--- modulus.rb
+++ modulus.rb
@@ -1,14 +1,14 @@
 
 def fizz_buss(int)
   if int%15 ==0
-puts "Fizz"
+    puts "Fizz"
   elsif int%5==0
     puts "Buzz"
   elsif int%3==0
-  puts "FizzBuzz"
-else
-  puts "nil" 
-end 
+   puts "FizzBuzz"
+  else
+    puts "nil" 
+  end 
 end
 
     
